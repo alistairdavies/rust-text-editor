35fb3f55039acc32a0dec672ce163fdb50c8fb14
Alistair Davies <ali17sam@gmail.com>
2022-09-27 22:11:01 +0100

    WIP

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+mod document;
+mod editor;
+mod row;
+mod terminal;
+pub use document::Document;
+pub use editor::Position;
+pub use row::Row;
+pub use terminal::Terminal;
+
+fn main() {
+    editor::Editor::default().run();
+}
